0cb9fc553d3804b1d027b646bf5309aeeba11bbb
entcheva <christina.entcheva@gmail.com>
2016-12-01 16:51:52 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -6,8 +6,37 @@ class Song
     @@all
   end
 
+
+  def self.create
+    self.new.save
+  end
+
+
+  def self.new_by_name(name)
+    song = self.new(name)
+    song
+  end
+
+
+  def self.create_by_name(name)
+    song = self.new(name).save
+    song
+  end
+
+
+  def self.find_by_name(name)
+    self.all.find{|element| element.name == name}
+  end
+
+
+  def initialize(name=nil)
+    @name = name
+  end
+
+
   def save
     self.class.all << self
+    self
   end
 
 end
